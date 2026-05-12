7386d545a4c9b82a2657332f963fef79496f2cee
robertooo <akidivarobertoo@gmail.com>
2026-05-12 07:26:24 +0300

    update index

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`)
 })
+app.get('/', (req, res) => {
+  res.send('Server is running perfectly!');
+});
