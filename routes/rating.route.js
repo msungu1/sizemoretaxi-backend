0b491f3c64a80a08a1c2e92dd6366197b02056a4
robertooo <akidivarobertoo@gmail.com>
2026-07-07 11:06:30 +0300

    “all”

--- routes/rating.route.js
+++ routes/rating.route.js
@@ -2,7 +2,6 @@ import express from "express"
 import { rateDriver, getDriverRatings } from "../controllers/rating.controller.js"
 const router = express.Router()
 
-import { rateDriver } from "../controllers/rating.controller.js"
 import { verifyToken } from "../middleware/verify.js"
 
 router.post("/", verifyToken, rateDriver);
